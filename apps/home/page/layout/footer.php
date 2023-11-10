549974afbcf7ad7352cb8d40f0809e8038e89e8d
manhcuong04 <cuongqc04@gmail.com>
2023-11-10 09:51:37 +0700

    ui home

--- apps/home/page/layout/footer.php
+++ apps/home/page/layout/footer.php
@@ -10,7 +10,7 @@
                     <img src="assets/img/footer-logo.png" alt="">
                   </a>
                 </div>
-                <p>We inspire and reach millions of travelers<br /> across 90 local websites</p>
+                <p>Chúng tôi truyền cảm hứng và tiếp cận hàng triệu khách du lịch<br /> trên 90 trang web địa phương</p>
                 <div class="fa-social">
                   <a href="#"><i class="fa fa-facebook"></i></a>
                   <a href="#"><i class="fa fa-twitter"></i></a>
@@ -22,18 +22,18 @@
             </div>
             <div class="col-lg-3 offset-lg-1">
               <div class="ft-contact">
-                <h6>Contact Us</h6>
+                <h6>Liên hệ chúng tôi</h6>
                 <ul>
-                  <li>(12) 345 67890</li>
+                  <li>0378453200</li>
                   <li>cuongqc04@gmail.com</li>
-                  <li>856 Cordia Extension Apt. 356, Lake, United State</li>
+                  <li>Cao Đẳng FPT Phố Trịnh Văn Bô, Phường Phương Canh, quận Nam Từ Liêm, Hà Nội</li>
                 </ul>
               </div>
             </div>
             <div class="col-lg-3 offset-lg-1">
               <div class="ft-newslatter">
-                <h6>New latest</h6>
-                <p>Get the latest updates and offers.</p>
+                <h6>Mới nhất </h6>
+                <p>Nhận thông tin cập nhật và ưu đãi mới nhất.</p>
                 <form action="#" class="fn-form">
                   <input type="text" placeholder="Email">
                   <button type="submit"><i class="fa fa-send"></i></button>
@@ -48,22 +48,15 @@
           <div class="row">
             <div class="col-lg-7">
               <ul>
-                <li><a href="#">Contact</a></li>
-                <li><a href="#">Terms of use</a></li>
-                <li><a href="#">Privacy</a></li>
-                <li><a href="#">Environmental Policy</a></li>
+                <li><a href="#">Liên hệ</a></li>
+                <li><a href="#">Điều khoản sử dụng</a></li>
+                <li><a href="#">Sự riêng tư</a></li>
+                <li><a href="#">Chính sách môi trường</a></li>
               </ul>
             </div>
             <div class="col-lg-5">
               <div class="co-text">
-                <p>
-                  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                  Copyright &copy;<script>
-                  document.write(new Date().getFullYear());
-                  </script> All rights reserved | This template is made with <i class="fa fa-heart"
-                    aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-                  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                </p>
+               
               </div>
             </div>
           </div>
